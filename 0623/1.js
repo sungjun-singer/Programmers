1bf49a295aafb1293b6d1461c296f04b9f8909ff
kimsungjun <kis07150@gmail.com>
2023-06-23 19:51:51 +0900

    23.06.23 공부-1

---
+++ 0623/1.js
@@ -0,0 +1,17 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = line.split(' ');
+}).on('close', function () {
+    let str = '';
+    for(let i = 0; i< Number(input); i++){
+        str += '*';
+        console.log(str);
+    }
+});
