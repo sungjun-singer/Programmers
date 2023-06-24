8d4633bbb9125136bee8cc2b4e6d8119b35e0c07
김성준 <kimseongjun@gimseongjun-ui-MacBookAir.local>
2023-06-24 23:06:10 +0900

    23.06.24 프로그래머스-3

--- 0624/16.js
+++ 0624/16.js
@@ -1,2 +1,12 @@
-console.log("hello");
-console.log("hi zsh");
+// 9로 나눈 나머지
+function solution(number) {
+    var answer = 0;
+    let sum = 0;
+    for(let i = 0; i<number.length; i++){
+        sum += parseInt(number[i]);
+    }
+    
+    answer = sum%9;
+    
+    return answer;
+}
